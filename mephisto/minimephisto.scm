7464543b5abc9453339d43ee4c7a01cc00e5256c
toru.gwaihir <toru.gwaihir@310e94c1-e14d-0410-9951-857b1988523a>
2008-06-03 09:32:30 +0000

    test ...

--- mephisto/minimephisto.scm
+++ mephisto/minimephisto.scm
@@ -3,6 +3,11 @@
 
 (define (mephisto-init!)
   (gl-clear-color 0.0 0.0 0.0 0.0)
+  (gl-matrix-mode GL_PROJECTION)
+  (gl-load-identity)
+  (gl-ortho -1.0 1.0 -1.0 1.0 -1.0 1.0)
+
+  (gl-clear-color 0.0 0.0 0.0 0.0)
   (gl-shade-model GL_FLAT)
 
   (gl-light GL_LIGHT0 GL_POSITION '#f32(0.0 10.0 20.0 0.0))
@@ -15,26 +20,51 @@
   (gl-enable GL_NORMALIZE)
 
   (gl-load-identity)
-  (glu-look-at 0.0 0.0 15.0 0.0 0.0 0.0 0.0 1.0 0.0)
+  (glu-look-at 0.5 0.0 1.0 0.0 0.0 0.0 0.0 1.0 0.0)
 
-  (glut-display-func display)
-  (glut-keyboard-func keyboard)
+;;   (glut-display-func display)
+;;   (glut-keyboard-func keyboard)
   )
 
 (define (display)
-  (display-cont)
+  (display-content)
   )
 
-(define (display-cont)
+(define (display-content)
+  (gl-clear GL_COLOR_BUFFER_BIT)
+  (gl-clear GL_DEPTH_BUFFER_BIT)
+
+  (gl-color '#f32(0 1.0 1.0))
+  (gl-begin* GL_POLYGON
+    (gl-vertex '#f32(0.25 0.25 0.0))
+    (gl-vertex '#f32(0.75 0.25 0.0))
+    (gl-vertex '#f32(0.75 0.75 0.0))
+    (gl-vertex '#f32(0.25 0.75 0.0))
+    )
+;;   (gl-flush)
+
+  (gl-color 0.0 1.0 0.0)
+
+  (gl-begin GL_TRIANGLE_STRIP)
+  (let loop ((n 50))
+    (gl-vertex (* n 0.003) (* 1 (sin (* n 0.3))) (* 1 (cos (* n 0.3))))
+    (if (< n 0)
+	'done
+	(loop (- n 1)))
+    )
+  (gl-end)
+
+  (glut-solid-torus 0.2 0.5 10 10)
+  (gl-flush)
   )
 
-(define (reshape w h)
-  (gl-viewport 0 0 w h)
-  (gl-matrix-mode GL_PROJECTION)
-  (gl-load-identity)
-  (gl-frustum -2.0 2.0 -1.5 1.5 1.5 30.0)
-  (gl-matrix-mode GL_MODELVIEW)
-  )
+;; (define (reshape w h)
+;;   (gl-viewport 0 0 w h)
+;;   (gl-matrix-mode GL_PROJECTION)
+;;   (gl-load-identity)
+;;   (gl-frustum -2.0 2.0 -1.5 1.5 1.5 30.0)
+;;   (gl-matrix-mode GL_MODELVIEW)
+;;   )
 
 (define (keyboard key x y)
   (case key
@@ -42,13 +72,18 @@
 
 (define (mephisto-main args)
   (glut-init args)
-  (glut-init-display-mode (logior GLUT_DOUBLE GLUT_DEPTH GLUT_RGB))
+  (glut-init-display-mode (logior GLUT_SINGLE GLUT_DEPTH GLUT_RGB))
 
-  (glut-init-window-size 320 240)
+  (glut-init-window-size 640 480)
   (glut-init-window-position 0 0)
   (glut-create-window "MEPHISTO")
   (mephisto-init!)
+
+  (glut-display-func display)
   (glut-keyboard-func keyboard)
 
   (glut-main-loop)
   0)
+
+(define (main args)
+  (mephisto-main args))
