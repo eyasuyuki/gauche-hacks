d7ae9e9a5d886d8f50339258ddc3ba990e5f3815
toru.gwaihir <toru.gwaihir@310e94c1-e14d-0410-9951-857b1988523a>
2008-06-17 10:10:23 +0000

    added mouse move handler

--- mephisto/minimephisto.scm
+++ mephisto/minimephisto.scm
@@ -26,7 +26,7 @@
 ;   (glu-look-at 0.5 0.0 1.0 0.0 0.0 0.0 0.0 1.0 0.0)
   )
 
-(define (display-content m)
+(define (display-content)
   (gl-clear (logior GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
 
   (gl-color 0.0 1.0 0.0)
@@ -35,13 +35,11 @@
   (gl-material GL_FRONT GL_AMBIENT
 	       (f32vector (/ 0 10) (/ 1 10) (/ 0 10) 1.0))
 
-  (gl-push-matrix)
-  (gl-rotate (* m 0.2) 0 (sin (* m 0.01)) (cos (* m 0.01)))
   (gl-begin GL_TRIANGLE_STRIP)
   (let loop ((n 100))
     (let ((r (+ 5(* n 0.03)))
-	  (theta (* n 0.3))
-	  (phi (* n 0.5)))
+	  (theta (* n 0.2))
+	  (phi (* n 0.7)))
       (let1 r-cos-phi (* r (cos phi))
 	(let ((x (* r-cos-phi (cos theta)))
 	      (y (* r-cos-phi (sin theta)))
@@ -53,7 +51,6 @@
 	(loop (- n 1)))
     )
   (gl-end)
-  (gl-pop-matrix)
 
   (glut-swap-buffers)
   )
@@ -70,15 +67,28 @@
       )
     ))
 
+(define *mouse-x* 0)
+(define *mouse-y* 0)
+(define *window-width* 640)
+(define *window-height* 480)
+
+(define passive-motion
+  (let ((w (/ *window-width* 2))
+	(h (/ *window-height* 2)))
+    (lambda (x y)
+      (set! *mouse-x* (* (/ (- x w) w) 2))
+      (set! *mouse-y* (* (/ (- y h) h) 2)))))
+
 (define (mephisto-main args)
   (glut-init args)
   (glut-init-display-mode (logior GLUT_DOUBLE GLUT_DEPTH GLUT_RGB))
 
-  (glut-init-window-size 640 480)
+  (glut-init-window-size *window-width* *window-height*)
   (glut-init-window-position 0 0)
   (glut-create-window "MEPHISTO")
   (mephisto-init!)
 
+  (glut-passive-motion-func passive-motion)
   (glut-display-func (make-display))
   (glut-idle-func (make-display))
   (glut-keyboard-func keyboard)
@@ -90,17 +100,31 @@
   (mephisto-main args))
 
 (define draw
-  (let1 time 0
+  (let ((time 0)
+	(ang-x 0)
+	(ang-y 0)
+	(ang-z 0))
     (lambda ()
-  (gl-matrix-mode GL_PROJECTION)
-  (gl-load-identity)
-  (glu-perspective 60 4/3 1 100)
+      (gl-matrix-mode GL_PROJECTION)
+      (gl-load-identity)
+      (glu-perspective 60 4/3 1 100)
 
-  (gl-matrix-mode GL_MODELVIEW)
+      (gl-matrix-mode GL_MODELVIEW)
       (gl-load-identity)
       (glu-look-at 0.0 0.0 -20.0 0.0 0.0 0.0 0.0 1.0 0.0)
-      (display-content time)
-      (inc! time))))
+
+      (gl-push-matrix)
+;       (gl-rotate (* time 0.2) 0 (sin (* time 0.01)) (cos (* time 0.01)))
+      (gl-rotate ang-x 1 0 0)
+      (gl-rotate ang-y 0 1 0)
+      (gl-rotate ang-z 0 0 1)
+      (display-content)
+      (gl-pop-matrix)
+
+      (inc! time)
+      (set! ang-x (- ang-x *mouse-y*))
+      (set! ang-y (+ ang-y *mouse-x*))
+      (set! ang-z (+ ang-z 0.1)))))
 
 (define make-anim-stream
   (stream-cons draw make-anim-stream))
